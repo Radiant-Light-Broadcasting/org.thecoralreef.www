6eb7b8ef39963b9395b894007e338bb8a7bd4a3f
radiant-light-broadcasting <106893231+eric-pirozok@users.noreply.github.com>
2023-06-27 09:35:48 -0700

    Superhero Organizations

--- js/book-names.js
+++ js/book-names.js
@@ -1,4 +1,5 @@
 var books = [
-  { name: "The Coral Reef", ref: "01-B001-001.html" },
+  { name: "About Us", ref: "01-B001-001.html" },
   { name: "What is it?", ref: "02-B002-001.html" },
+  { name: "Superhero Organizations", ref: "03-B003-001.html" },
 ];
